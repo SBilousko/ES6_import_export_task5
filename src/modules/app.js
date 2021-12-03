1cf0f1a31eb8c4272046147c03162b15ceb034d3
SBilousko <s.bilousko@ukr.net>
2021-12-04 01:40:09 +0200

    Task 5 Fixes

--- src/modules/app.js
+++ src/modules/app.js
@@ -1,13 +1,6 @@
 import { DonateForm } from "./donate-form";
 import { DonateList } from "./donate-list";
 
-// const mockDonates = [
-//   { amount: 4, date: new Date() },
-//   { amount: 20, date: new Date() },
-//   { amount: 3, date: new Date() },
-//   { amount: 1, date: new Date() },
-// ];
-
 export default class App {
   #formBlock;
   #donateList;
